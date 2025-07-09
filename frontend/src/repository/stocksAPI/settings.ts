ff45d6b76569e22b9ee95a36a8428b51193b39de
Julian Adolfo Vega Guzman <ja.vega.g10@gmail.com>
2025-07-08 19:42:21 -0500

    chore(frontend/apiClient): add repository to handle fetching the API

---
+++ frontend/src/repository/stocksAPI/settings.ts
@@ -0,0 +1,8 @@
+import type { IEndpoints } from './interfaces/settings'
+
+export const baseURL: string = import.meta.env.VITE_API_URL
+
+export const endpoints: IEndpoints = {
+  get: `${baseURL}/api/stocks`,
+  getRecommendations: `${baseURL}/api/stocks/recommendations`,
+}
---
+++ frontend/src/repository/stocksAPI/stockService.ts
@@ -0,0 +1,69 @@
+import type { IFilterOptions } from './interfaces/settings'
+import type { IStockListResponse } from './interfaces/stocks'
+import { endpoints } from './settings'
+
+export async function getRecommendations(
+  limit?: number,
+  offset: number = 0,
+  filterOptions?: IFilterOptions,
+): Promise<IStockListResponse> {
+  let url = `${endpoints.get}?offset=${offset}`
+
+  if (limit) {
+    url += `&limit=${limit}`
+  }
+
+  if (filterOptions?.Company) {
+    url += `&company=${filterOptions?.Company}`
+  }
+
+  if (filterOptions?.RatingTo) {
+    url += `&rating-to=${filterOptions?.RatingTo}`
+  }
+
+  if (filterOptions?.TargetToMin) {
+    url += `&target-to-min=${filterOptions?.TargetToMin}`
+  }
+
+  if (filterOptions?.TargetToMax) {
+    url += `&target-to-max=${filterOptions?.TargetToMax}`
+  }
+
+  const res = await fetch(url)
+  const json = await res.json()
+
+  return json.data as IStockListResponse
+}
+
+export async function getAll(
+  limit?: number,
+  offset: number = 0,
+  filterOptions?: IFilterOptions,
+): Promise<IStockListResponse> {
+  let url = `${endpoints.get}?offset=${offset}`
+
+  if (limit) {
+    url += `&limit=${limit}`
+  }
+
+  if (filterOptions?.Company) {
+    url += `&company=${filterOptions?.Company}`
+  }
+
+  if (filterOptions?.RatingTo) {
+    url += `&rating-to=${filterOptions?.RatingTo}`
+  }
+
+  if (filterOptions?.TargetToMin) {
+    url += `&target-to-min=${filterOptions?.TargetToMin}`
+  }
+
+  if (filterOptions?.TargetToMax) {
+    url += `&target-to-max=${filterOptions?.TargetToMax}`
+  }
+
+  const res = await fetch(url)
+  const json = await res.json()
+
+  return json.data as IStockListResponse
+}
