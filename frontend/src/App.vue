2dc5be3cec4b237c95cab142c838ed6b85298c22
Julian Adolfo Vega Guzman <ja.vega.g10@gmail.com>
2025-07-09 19:21:28 -0500

    feat(frontend/main): add routing and create the actual app

---
+++ frontend/src/App.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="bg-white py-4 shadow">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/recommendations">Recommendations</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100dvh;
+}
+
+.wrapper {
+  width: 100%;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    width: 100%;
+  }
+
+  nav {
+    text-align: left;
+    font-size: 1rem;
+    padding: 1rem 0;
+  }
+}
+</style>
